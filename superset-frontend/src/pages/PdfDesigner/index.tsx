f6e7f93d87e211706b7f86eaf7902900fe4fa5b7
Jawadh Salih Rifath <user@Jawadhs-Mac.local>
2025-02-01 20:12:24 +0530

    Adding more template usages

--- superset-frontend/src/pages/PdfDesigner/index.tsx
+++ superset-frontend/src/pages/PdfDesigner/index.tsx
@@ -6,10 +6,4 @@ import PdfmeDesignerComponent from './PdfmeDesignerComponent';
 export default function App() {
 
   return <PdfmeDesignerComponent/>;
-  // <div>
-  //   <h1>Main React App</h1>
-  //   <Suspense fallback={<div>Loading Microfrontend...</div>}>
-  //     <PdfmeDesignerComponent />
-  //   </Suspense>
-  // </div>);
 }
