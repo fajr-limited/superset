9e8bee18f940c71fe05693caf946439c01a1de98
Jawadh Salih Rifath <user@Jawadhs-Mac.local>
2025-02-01 20:12:24 +0530

    Adding more template usages

--- superset-frontend/src/pages/PdfDesigner/PdfmeDesignerComponent.tsx
+++ superset-frontend/src/pages/PdfDesigner/PdfmeDesignerComponent.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useRef } from 'react';
 import { Designer } from '@pdfme/ui';
-import type { Template } from '@pdfme/common';
-import { getTemplate } from './helper';
+import { getInputFromTemplate, type Template } from '@pdfme/common';
+import { getTemplate, getTemplatePlugins } from './helper';
+import { generate } from "@pdfme/generator";
+import { text, image, barcodes } from "@pdfme/schemas";
 
 const PdfMeDesignerComponent = () => {
   // Create a reference to store the Designer instance
@@ -20,7 +22,8 @@ const PdfMeDesignerComponent = () => {
       // Create and store the Designer instance in the ref
       designerRef.current = new Designer({
         domContainer: domContainerRef.current,
-        template
+        template,
+        plugins: getTemplatePlugins(),
       });
 
       // Optionally, you can call methods on the designer here, for example:
@@ -42,8 +45,36 @@ const PdfMeDesignerComponent = () => {
     };
   }, [template]); // Update Designer if template changes
 
+  const downloadPDF = async () => {
+    if (!designerRef || !designerRef.current) return;
+
+    // Get the updated template from the Designer UI
+    const updatedTemplate = designerRef.current.getTemplate();
+
+    // Insert user input data into the PDF fields
+    // const inputs = [formData];
+    console.log(updatedTemplate);
+
+    const plugins = getTemplatePlugins();
+    const inputs = getInputFromTemplate(updatedTemplate);
+    // Generate PDF
+    const pdfBuffer = await generate({ template: updatedTemplate, inputs, plugins });
+
+    // Create a download link
+    const blob = new Blob([pdfBuffer], { type: "application/pdf" });
+    window.open(URL.createObjectURL(blob));
+    // const link = document.createElement("a");
+    // link.href = URL.createObjectURL(blob);
+
+    // link.download = "customized.pdf";
+    // document.body.appendChild(link);
+    // link.click();
+    // document.body.removeChild(link);
+  };
+
   return (
-    <div>
+    <div id='container'>
+      <button onClick={downloadPDF} className="bg-blue-500 text-white px-4 py-2 mt-4">Download PDF</button>
       <div id="container" ref={domContainerRef} style={{ width: '100%', height: '100vh', backgroundColor: 'lightgray' }}>
         {/* You can add other content or components as needed */}
       </div>
