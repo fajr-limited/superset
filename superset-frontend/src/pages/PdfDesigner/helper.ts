b549248efa52c9d491a71c41d09efd7f6a25b02f
Imaadh Ifthikar <101471526+imaadh0@users.noreply.github.com>
2025-02-24 20:33:32 +0530

    Added the dimensions for the pdf

--- superset-frontend/src/pages/PdfDesigner/helper.ts
+++ superset-frontend/src/pages/PdfDesigner/helper.ts
@@ -1,4 +1,4 @@
-import { Template, checkTemplate, BLANK_PDF } from "@pdfme/common";
+import { Template, checkTemplate, BLANK_PDF } from '@pdfme/common';
 import {
   multiVariableText,
   text,
@@ -19,21 +19,21 @@ import {
 
 export const readFile = (
   file: File | null,
-  type: "text" | "dataURL" | "arrayBuffer"
+  type: 'text' | 'dataURL' | 'arrayBuffer',
 ) => {
-  return new Promise<string | ArrayBuffer>((r) => {
+  return new Promise<string | ArrayBuffer>(r => {
     const fileReader = new FileReader();
-    fileReader.addEventListener("load", (e) => {
+    fileReader.addEventListener('load', e => {
       if (e && e.target && e.target.result && file !== null) {
         r(e.target.result);
       }
     });
     if (file !== null) {
-      if (type === "text") {
+      if (type === 'text') {
         fileReader.readAsText(file);
-      } else if (type === "dataURL") {
+      } else if (type === 'dataURL') {
         fileReader.readAsDataURL(file);
-      } else if (type === "arrayBuffer") {
+      } else if (type === 'arrayBuffer') {
         fileReader.readAsArrayBuffer(file);
       }
     }
@@ -43,7 +43,7 @@ export const readFile = (
 export const cloneDeep = (obj: any) => JSON.parse(JSON.stringify(obj));
 
 export const getTemplateFromJsonFile = (file: File) => {
-  return readFile(file, "text").then((jsonStr) => {
+  return readFile(file, 'text').then(jsonStr => {
     const template: Template = JSON.parse(jsonStr as string);
     try {
       checkTemplate(template);
@@ -55,12 +55,12 @@ export const getTemplateFromJsonFile = (file: File) => {
 };
 
 export const downloadJsonFile = (json: any, title: string) => {
-  if (typeof window !== "undefined") {
+  if (typeof window !== 'undefined') {
     const blob = new Blob([JSON.stringify(json)], {
-      type: "application/json"
+      type: 'application/json',
     });
     const url = URL.createObjectURL(blob);
-    const link = document.createElement("a");
+    const link = document.createElement('a');
     link.href = url;
     link.download = `${title}.json`;
     link.click();
@@ -79,53 +79,53 @@ export const isJsonString = (str: string) => {
 
 export function getTemplate(): Template {
   const template: Template = {
-    basePdf: BLANK_PDF,
+    basePdf: { width: 210, height: 297, padding: [10, 10, 10, 10] },
     schemas: [
       [
         {
-          "name": "name",
-          "type": "text",
-          "content": "Pet Name",
-          "position": { "x": 24.8, "y": 26.61 },
-          "width": 77.77,
-          "height": 18.7,
-          "fontSize": 36,
-          "fontColor": "#14b351"
+          name: 'name',
+          type: 'text',
+          content: 'Pet Name',
+          position: { x: 24.8, y: 26.61 },
+          width: 77.77,
+          height: 18.7,
+          fontSize: 36,
+          fontColor: '#14b351',
         },
         {
-          "name": "age",
-          "type": "text",
-          "content": "4 years",
-          "position": { "x": 36, "y": 179.46 },
-          "width": 43.38,
-          "height": 6.12,
-          "fontSize": 12
-        }
+          name: 'age',
+          type: 'text',
+          content: '4 years',
+          position: { x: 36, y: 179.46 },
+          width: 43.38,
+          height: 6.12,
+          fontSize: 12,
+        },
       ],
-      [ 
+      [
         {
-          "name": "sex",
-          "type": "text",
-          "content": "Male",
-          "position": { "x": 36, "y": 186.23 },
-          "width": 43.38,
-          "height": 6.12,
-          "fontSize": 12
+          name: 'sex',
+          type: 'text',
+          content: 'Male',
+          position: { x: 36, y: 186.23 },
+          width: 43.38,
+          height: 6.12,
+          fontSize: 12,
         },
         {
-          "name": "weight",
-          "type": "text",
-          "content": "33 pounds",
-          "position": { "x": 40, "y": 192.99 },
-          "width": 43.38,
-          "height": 6.12,
-          "fontSize": 12
-        }
-      ]
-    ]
+          name: 'weight',
+          type: 'text',
+          content: '33 pounds',
+          position: { x: 40, y: 192.99 },
+          width: 43.38,
+          height: 6.12,
+          fontSize: 12,
+        },
+      ],
+    ],
   };
   return template;
-};
+}
 
 export function getTemplatePlugins() {
   return {
@@ -156,4 +156,4 @@ export function getTemplatePlugins() {
     // UPCE: barcodes.upce,
     // GS1DataMatrix: barcodes.gs1datamatrix,
   };
-};
+}
