e28ac1b1bf9785aaedc0ea760f3aa3b94d4c1434
Jawadh Salih Rifath <user@Jawadhs-Mac.local>
2025-03-11 21:13:01 +0530

    Remove import

--- superset-frontend/src/views/routes.tsx
+++ superset-frontend/src/views/routes.tsx
@@ -21,7 +21,6 @@ import { lazy, ComponentType, ComponentProps } from 'react';
 
 // not lazy loaded since this is the home page.
 import Home from 'src/pages/Home';
-import PdfTemplateList from 'src/pages/PdfTemplateList';
 
 const ChartCreation = lazy(
   () =>
@@ -225,7 +224,7 @@ export const routes: Routes = [
   {
     path: '/sqllab/',
     Component: SqlLab,
-  }
+  },
 ];
 
 if (isFeatureEnabled(FeatureFlag.TaggingSystem)) {
